4631e8a59b102d0716f75565b6573edec541be31
zero228x <58635173+zero228x@users.noreply.github.com>
2020-02-27 20:41:18 +0300

    RETY

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1582737900,
+	"version": 1582825155,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -24,6 +24,16 @@
 		"images/sprite6-sheet0.png",
 		"images/sprite7-sheet0.png",
 		"images/sprite8-sheet0.png",
+		"images/sprite9-sheet0.png",
+		"images/sprite10-sheet0.png",
+		"images/sprite11-sheet0.png",
+		"images/sprite12-sheet0.png",
+		"images/sprite13-sheet0.png",
+		"images/sprite14-sheet0.png",
+		"images/sprite15-sheet0.png",
+		"images/sprite15-sheet1.png",
+		"images/tiledbackground8.png",
+		"images/tiledbackground9.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
