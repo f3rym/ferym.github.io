b8ae63555741976db70f80ca58d8946d575deb26
zero228x <58635173+zero228x@users.noreply.github.com>
2020-05-29 11:37:06 +0300

    магазин на react-redux

---
+++ build/precache-manifest.5198bcaaeeec0e7fe2c0fc568fab22cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5538b351698ea726ad59653c33129241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2955eae5f5c24d470ec1",
+    "url": "/static/css/main.8d0f7cf6.chunk.css"
+  },
+  {
+    "revision": "7d8f01e35bf003221916",
+    "url": "/static/js/2.c63538c2.chunk.js"
+  },
+  {
+    "revision": "2955eae5f5c24d470ec1",
+    "url": "/static/js/main.b965867f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
